c7d2a333a77327ee14b1fd67b1235ed57445ea36
André Ferreira <anfreire.dev@gmail.com>
2024-08-07 10:51:17 +0100

    2024/08/07

--- App.tsx
+++ App.tsx
@@ -5,78 +5,84 @@
  * @format
  */
 
-import React, {useEffect} from 'react';
-import {SafeAreaView, StatusBar, View} from 'react-native';
-import Pages from '@/pages';
-import FilesModule from '@/lib/files';
-import {useTheme} from '@/theme';
-import DrawerWrapper from '@/global/drawer';
-import {useSettings} from '@/states/persistent/settings';
-import {useDialogs} from '@/states/temporary/dialogs';
-import {useApp} from '@/states/temporary/app';
-import {useTips} from '@/states/temporary/tips';
-import PermissionsModule from '@/lib/permissions';
-import { initBackgroundTasks } from '@/lib/background';
-import { useToken } from '@/states/persistent/token';
+import React, { useEffect } from "react";
+import { SafeAreaView, StatusBar, View } from "react-native";
+import Pages from "@/pages";
+import FilesModule from "@/lib/files";
+import { useTheme } from "@/theme";
+import DrawerWrapper from "@/global/drawer";
+import { useSettings } from "@/states/persistent/settings";
+import { useDialogs } from "@/states/temporary/dialogs";
+import { useApp } from "@/states/temporary/app";
+import { useTips } from "@/states/temporary/tips";
+import PermissionsModule from "@/lib/permissions";
+import { initBackgroundTasks } from "@/lib/background";
+import { useToken } from "@/states/persistent/token";
 
 function App(): React.JSX.Element {
-  const theme = useTheme();
-  const deleteOnLeave = useSettings(
-    state => state.settings.downloads.deleteOnLeave,
-  );
-  const [info, localVersion] = useApp(state => [
-    state.info,
-    state.localVersion,
-  ]);
-  const openDialog = useDialogs().openDialog;
-  const fetchTips = useTips().fetchTips;
-  const [releaseNotification, updateNotification] = useSettings(state => [
-    state.settings.notifications.newReleaseNotification,
-    state.settings.notifications.updatesNotification,
-  ]);
-    
+	const theme = useTheme();
+	const deleteOnLeave = useSettings(
+		(state) => state.settings.downloads.deleteOnLeave,
+	);
+	const openDialog = useDialogs().openDialog;
+	const fetchTips = useTips().fetchTips;
+	const initToken = useToken().init;
+	const [info, localVersion] = useApp((state) => [
+		state.info,
+		state.localVersion,
+	]);
+	const [releaseNotification, updateNotification] = useSettings((state) => [
+		state.settings.notifications.newReleaseNotification,
+		state.settings.notifications.updatesNotification,
+	]);
 
-  useEffect(() => {
-    if (info.version && localVersion && info.version > localVersion)
-      openDialog('newVersion');
-  }, [info]);
+	useEffect(() => {
+		if (localVersion && info.version && info.version > localVersion)
+			openDialog("newVersion");
+	}, [info, localVersion]);
 
-  useEffect(() => {
-    useToken.getState().init();
-    if (releaseNotification || updateNotification) {
-      PermissionsModule.grantPostNotification().then(_ =>
-        initBackgroundTasks(),
-      );
-    }
-    const fun = () => deleteOnLeave && FilesModule.deleteAllFiles();
-    fun();
-    fetchTips();
-    return () => {
-      fun();
-    };
-  }, []);
+	useEffect(() => {
+		if (releaseNotification || updateNotification) {
+			PermissionsModule.grantPostNotification().then((_) =>
+				initBackgroundTasks(),
+			);
+		}
+	}, [releaseNotification, updateNotification]);
 
-  return (
-    <>
-      <StatusBar
-        backgroundColor={theme.schemedTheme.surfaceContainer}
-        barStyle={
-          theme.colorScheme === 'dark' ? 'light-content' : 'dark-content'
-        }
-      />
-      <SafeAreaView>
-        <View
-          style={{
-            width: '100%',
-            height: '100%',
-          }}>
-          <DrawerWrapper>
-            <Pages />
-          </DrawerWrapper>
-        </View>
-      </SafeAreaView>
-    </>
-  );
+	useEffect(() => {
+		initToken();
+		fetchTips();
+		const deleteOnLeaveCallback = () => {
+			if (deleteOnLeave) FilesModule.deleteAllFiles();
+		};
+
+		return deleteOnLeaveCallback;
+	}, []);
+
+	return (
+		<>
+			<StatusBar
+				backgroundColor={theme.schemedTheme.surfaceContainer}
+				barStyle={
+					theme.colorScheme === "dark"
+						? "light-content"
+						: "dark-content"
+				}
+			/>
+			<SafeAreaView>
+				<View
+					style={{
+						width: "100%",
+						height: "100%",
+					}}
+				>
+					<DrawerWrapper>
+						<Pages />
+					</DrawerWrapper>
+				</View>
+			</SafeAreaView>
+		</>
+	);
 }
 
 export default App;
--- index.js
+++ index.js
@@ -2,28 +2,30 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-import 'react-native-gesture-handler';
-import {ThemeProvider} from '@/theme';
-import {ToastWrapper} from '@/global/toast';
-import {DialogsWrapper} from '@/global/dialogs';
-import BackgroundFetch from 'react-native-background-fetch';
-import {headlessTask} from '@/lib/background';
+import { AppRegistry } from "react-native";
+import App from "./App";
+import { name as appName } from "./app.json";
+import "react-native-gesture-handler";
+import { ThemeProvider } from "@/theme";
+import { ToastWrapper } from "@/global/toast";
+import { DialogsWrapper } from "@/global/dialogs";
+import BackgroundFetch from "react-native-background-fetch";
+import { headlessTask } from "@/lib/background";
+import { ComputedProvider } from "@/providers/computed";
 
 BackgroundFetch.registerHeadlessTask(headlessTask);
 function Root() {
-  return (
-    <ThemeProvider>
-      <ToastWrapper>
-        <DialogsWrapper>
-          <App />
-        </DialogsWrapper>
-      </ToastWrapper>
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider>
+			<ComputedProvider>
+				<ToastWrapper>
+					<DialogsWrapper>
+						<App />
+					</DialogsWrapper>
+				</ToastWrapper>
+			</ComputedProvider>
+		</ThemeProvider>
+	);
 }
 
 AppRegistry.registerComponent(appName, () => Root);
-
