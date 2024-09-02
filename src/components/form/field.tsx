a5fee0ee0465c06494704d8eac4310c45faa1d2b
André Ferreira <anfreire.dev@gmail.com>
2024-09-02 14:48:12 +0100

    v3.0.0 - 02/09/2024

--- src/components/form/field.tsx
+++ src/components/form/field.tsx
@@ -3,96 +3,96 @@ import { StyleSheet, View } from "react-native";
 import { TextInput } from "react-native-paper";
 import { ScrollView } from "react-native";
 import FormSuggestions from "./suggestions";
+import isEqual from "react-fast-compare";
 
-interface FormFieldsProps {
-  label: string;
-  suggestions?: string[];
-  fieldKey: string;
-  value: string;
-  error: boolean;
-  onChange: (field: string, value: string) => void;
-  disabled: boolean;
-  numberOfLines?: number;
-  scrollTo?: (y: number) => void;
-  scrollViewRef?: React.RefObject<ScrollView>;
+interface FormFieldProps {
+	label: string;
+	suggestions?: string[];
+	fieldKey: string;
+	value: string;
+	error: boolean;
+	onChange: (field: string, value: string) => void;
+	disabled: boolean;
+	numberOfLines?: number;
+	scrollTo?: (y: number) => void;
+	scrollViewRef?: React.RefObject<ScrollView>;
 }
 
 const FormField = ({
-  label,
-  suggestions,
-  fieldKey,
-  value,
-  error,
-  onChange,
-  disabled,
-  numberOfLines = 1,
-  scrollTo,
-  scrollViewRef,
-}: FormFieldsProps) => {
-  const viewRef = React.useRef<View>(null);
+	label,
+	suggestions,
+	fieldKey,
+	value,
+	error,
+	onChange,
+	disabled,
+	numberOfLines = 1,
+	scrollTo,
+	scrollViewRef,
+}: FormFieldProps) => {
+	const viewRef = React.useRef<View>(null);
 
-  const onFocused = React.useCallback(() => {
-    viewRef.current?.measureLayout(
-      scrollViewRef?.current?.getInnerViewNode(),
-      (_, y) => {
-        scrollTo?.(y - 150);
-      }
-    );
-  }, [scrollTo]);
+	const onFocused = React.useCallback(() => {
+		viewRef.current?.measureLayout(
+			scrollViewRef?.current?.getInnerViewNode(),
+			(_, y) => {
+				scrollTo?.(y - 150);
+			},
+		);
+	}, [scrollTo, scrollViewRef]);
 
-  const multilineProps = React.useMemo(
-    () => ({
-      multiline: numberOfLines > 1,
-      maxLength: numberOfLines > 1 ? 100 : 100,
-      minHeight: numberOfLines > 1 ? 150 : undefined,
-    }),
-    [numberOfLines]
-  );
+	const multilineProps = React.useMemo(
+		() => ({
+			multiline: numberOfLines > 1,
+			minHeight: numberOfLines > 1 ? 150 : undefined,
+		}),
+		[numberOfLines],
+	);
 
-  return (
-    <View style={syles.wrapper} ref={viewRef}>
-      <TextInput
-        mode="outlined"
-        onFocus={onFocused}
-        label={label}
-        disabled={disabled}
-        editable={!disabled}
-        multiline={multilineProps.multiline}
-        numberOfLines={1}
-        maxLength={multilineProps.maxLength}
-        value={value}
-        onChangeText={(text) => onChange(fieldKey, text)}
-        style={[syles.input, { minHeight: multilineProps.minHeight }]}
-        error={error}
-      />
-      <FormSuggestions
-        fieldKey={fieldKey}
-        onChange={onChange}
-        suggestions={suggestions}
-      />
-    </View>
-  );
+	return (
+		<View style={syles.wrapper} ref={viewRef}>
+			<TextInput
+				mode="outlined"
+				onFocus={onFocused}
+				label={label}
+				disabled={disabled}
+				editable={!disabled}
+				multiline={multilineProps.multiline}
+				numberOfLines={1}
+				maxLength={100}
+				value={value}
+				onChangeText={(text) => onChange(fieldKey, text)}
+				style={[syles.input, { minHeight: multilineProps.minHeight }]}
+				error={error}
+			/>
+			<FormSuggestions
+				fieldKey={fieldKey}
+				onChange={onChange}
+				suggestions={suggestions}
+			/>
+		</View>
+	);
 };
 
 const syles = StyleSheet.create({
-  wrapper: {
-    width: "100%",
-  },
-  input: {
-    width: "100%",
-  },
-  suggestions: {
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-    flexWrap: "wrap",
-    marginBottom: 10,
-    marginTop: 5,
-    gap: 5,
-  },
+	wrapper: {
+		width: "100%",
+	},
+	input: {
+		width: "100%",
+	},
+	suggestions: {
+		display: "flex",
+		flexDirection: "row",
+		alignItems: "center",
+		justifyContent: "center",
+		flexWrap: "wrap",
+		marginBottom: 10,
+		marginTop: 5,
+		gap: 5,
+	},
 });
 
 FormField.displayName = "FormField";
 
-export default React.memo(FormField);
+export default React.memo(FormField, isEqual);
--- src/global/dialogs/components/custom.tsx
+++ src/global/dialogs/components/custom.tsx
@@ -1,38 +1,44 @@
 import * as React from "react";
 import { Button, Dialog, Text } from "react-native-paper";
 import { useDialogs } from "@/states/runtime/dialogs";
+import { useShallow } from "zustand/react/shallow";
 
 const CustomDialog = () => {
-  const [activeDialog, closeDialog, customDialog] = useDialogs((state) => [
-    state.activeDialog,
-    state.closeDialog,
-    state.customDialog,
-  ]);
+	const [activeDialog, customDialog, closeDialog] = useDialogs(
+		useShallow((state) => [
+			state.activeDialog,
+			state.customDialog,
+			state.closeDialog,
+		]),
+	);
 
-  const handleActionPress = React.useCallback((action: () => void) => {
-    action();
-    closeDialog();
-  }, []);
+	const handleActionPress = React.useCallback((action: () => void) => {
+		action();
+		closeDialog();
+	}, []);
 
-  if (activeDialog !== "custom" || !customDialog) {
-    return null;
-  }
+	if (activeDialog !== "custom" || !customDialog) {
+		return null;
+	}
 
-  return (
-    <Dialog visible onDismiss={closeDialog}>
-      <Dialog.Title>{customDialog?.title}</Dialog.Title>
-      <Dialog.Content>
-        <Text variant="bodyMedium">{customDialog?.content}</Text>
-      </Dialog.Content>
-      <Dialog.Actions>
-        {customDialog?.actions.map((action, index) => (
-          <Button key={index} onPress={() => handleActionPress(action.action)}>
-            {action.title}
-          </Button>
-        ))}
-      </Dialog.Actions>
-    </Dialog>
-  );
+	return (
+		<Dialog visible onDismiss={closeDialog}>
+			<Dialog.Title>{customDialog?.title}</Dialog.Title>
+			<Dialog.Content>
+				<Text variant="bodyMedium">{customDialog?.content}</Text>
+			</Dialog.Content>
+			<Dialog.Actions>
+				{customDialog?.actions.map((action, index) => (
+					<Button
+						key={index}
+						onPress={() => handleActionPress(action.action)}
+					>
+						{action.title}
+					</Button>
+				))}
+			</Dialog.Actions>
+		</Dialog>
+	);
 };
 
 CustomDialog.displayName = "CustomDialog";
